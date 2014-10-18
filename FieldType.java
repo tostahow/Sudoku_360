f31de118f810c18e50c0f979cf1c517432834cf9
Travis Ostahowski <tostahowski@gmail.com>
2014-10-18 03:47:33 -0700

    Create FieldType.java

---
+++ FieldType.java
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------------------------------
+ * Document:
+ * 		FieldType.java
+ * 
+ * Description:
+ * 		Enumeration that helps describe different text fields encountered during
+ * 		Sudoku software run time.
+-------------------------------------------------------------------------------------------------*/
+public enum FieldType
+{
+	USERNAME,
+	PASSWORD,
+	CELL9,
+	CELL16
+}
